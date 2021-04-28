ca779e332f46db2c640fa062f7b5332db10e9ed7
leandro souza <leandro_a_souza@hotmail.com>
2021-04-28 11:09:15 -0300

    Metodos PUT, POST e DELETE Spring

--- Blog-Pessoal/src/main/java/com/generation/BlogPessoal/model/Postagem.java
+++ Blog-Pessoal/src/main/java/com/generation/BlogPessoal/model/Postagem.java
@@ -9,9 +9,9 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.sun.istack.NotNull;
 @Entity
 @Table(name = "Postagem")
 public class Postagem {
